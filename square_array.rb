0e215ad5c0aa1115a61726739f2787bea680722f
Ray's Git <lancelot53.ra@gmail.com>
2021-12-02 21:02:02 -0500

    Completed

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
+def square_array(numbers)
   # your code here
+  array_new =[]
+  numbers.each do|number|
+      array_new << number*number
+
+    end
+    array_new
 end
+#puts "collect numbers : #{numbers.collect {|x| x * x }}\n\n"
+
+#puts "collect a : #{a.collect {|x| x + 1 }}\n\n"
